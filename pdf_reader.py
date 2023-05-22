97c4297ee9d35d68af12af05cde4bd91c7c08c1c
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-05-22 12:06:21 +0100

    Update pdf_reader.py

--- pdf_reader.py
+++ pdf_reader.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import fitz  # PyMuPDF
 from PIL import Image
+import pandas as pd
 
 st.set_page_config(
     page_title="PDF Reader",
@@ -20,6 +21,12 @@ def convert_pdf_to_images(file):
 
     return images
 
+def display_historical_records(file):
+    if file is not None:
+        # Read the file using appropriate library or method
+        # For example, if it's a CSV file, you can use pandas
+        df = pd.read_csv(file)
+        st.write(df)  # Display the DataFrame on the Streamlit app
 
 def main():
     st.title("PDF Reader")
@@ -31,11 +38,14 @@ def main():
         for idx, image in enumerate(images):
             st.image(image, caption=f"Page {idx + 1}")
 
+    if st.button("View Historical Records"):
+        historical_records_file = st.file_uploader("Upload historical records file", type=["csv", "xlsx"])
+        display_historical_records(historical_records_file)
 
 if __name__ == "__main__":
     main()
+
 st.write("---")
-st.write("This page is reserved. All rights reserved. :copyright:")
+st.write("This page is reserved. All rights reserved. ©")
 
-st.markdown("Credit : [Jasser](https://www.facebook.com/jasser.razzek.3/)")
-
+st.markdown("Credit: [Jasser](https://www.facebook.com/jasser.razzek.3/)")
